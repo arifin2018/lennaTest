5d090b46e600e991df5b1d0e63d4ff506b83351e
arifin2018 <arifingdr@gmail.com>
2022-01-15 10:45:06 +0700

    pusing

--- app/Http/Controllers/TelegramBot.php
+++ app/Http/Controllers/TelegramBot.php
@@ -26,9 +26,9 @@ class TelegramBot extends Controller
         $telegram = new Api(env('TELEGRAM_BOT_TOKEN'));
 
         // $updates = $telegram->setWebHook("https://tesbotlenna.herokuapp.com");
-        $updates = $telegram->getWebhookUpdate();
+        $updates = $telegram->getUpdates();
 
-        return $updates['message'];
+        return $updates;
 
         if(isset($updates['message'])){
             $text = $updates['message']['text'];
