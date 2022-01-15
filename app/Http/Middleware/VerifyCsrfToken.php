7fced267a483c46beca175be8e02008d63c6a3b5
arifin2018 <arifingdr@gmail.com>
2022-01-15 07:18:51 +0700

    test ketujuh

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        '5025800924:AAG6ghT_u5HOqtghJaLcaODTOPn0RjP8rWk/webhook'
     ];
 }
