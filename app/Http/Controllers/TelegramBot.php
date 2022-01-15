4fec9c6dc5104713b6a14413b1fa7ecff59dffa3
arifin2018 <arifingdr@gmail.com>
2022-01-15 10:28:25 +0700

    test keseduabelas

--- app/Http/Controllers/TelegramBot.php
+++ app/Http/Controllers/TelegramBot.php
@@ -27,7 +27,6 @@ class TelegramBot extends Controller
 
         // $updates = $telegram->setWebHook("https://tesbotlenna.herokuapp.com");
         $updates = $telegram->getWebhookUpdate();
-        return $updates;
 
         if(isset($updates['message'])){
             $text = $updates['message']['text'];
