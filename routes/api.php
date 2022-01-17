4069f869bd11ddc39d7f4d0c432468531321dbc3
arifin2018 <arifingdr@gmail.com>
2022-01-17 07:26:53 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,6 @@ Route::post('register', [AuthController::class, 'register'])->name('register');
 Route::post('login', [AuthController::class, 'login'])->name('login');
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    // API route for logout user
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 });
 
